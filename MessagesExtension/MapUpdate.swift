9c3ed4e63d3a685779e6b8593ac056977ebbe28a
taiyo <taiyo@taiyos-MacBook-Pro.local>
2017-06-04 10:52:06 -0700

    Remove unused var declarations. Update centerView(). Create refreshMapView() Create displaUpdate().

--- MessagesExtension/MapUpdate.swift
+++ MessagesExtension/MapUpdate.swift
@@ -17,31 +17,13 @@ struct PointAnnotations {
 }
 
 class MapUpdate {
-    private var locLatitude: CLLocationDegrees = 0.0
-    private var locLongitude: CLLocationDegrees = 0.0
-    private var remLatitude: CLLocationDegrees = 0.0
-    private var remLongitude: CLLocationDegrees = 0.0
-    private var latDistTo: Double = 0.0
-    private var lngDistTo: Double = 0.0
-    private var centerLatitude: CLLocationDegrees = 0.0
-    private var centerLongitude: CLLocationDegrees = 0.0
-    public var eta: TimeInterval? = nil
-    public var distance: Double = 0.0
     public var pointAnnotationStruct = PointAnnotations()
-    
-    func showRemote (packet: Location, mapView: MKMapView) {
-        
-    }
 
     //func addPin (packet: Location, mapView: MKMapView, _ remove: Bool) -> MKPointAnnotation {
-    func addPin (packet: Location, mapView: MKMapView, _ remove: Bool) {
+    func addPin (packet: Location, mapView: MKMapView, remove: Bool) {
     
         print("-- MapUpdate -- addPin: add pin for remoteUser")
     
-        self.locLatitude  = packet.latitude
-        self.locLongitude = packet.longitude
-        self.remLatitude  = packet.remoteLatitude
-        self.remLongitude = packet.remoteLongitude
         let pointAnnotation: MKPointAnnotation
         
         pointAnnotation = MKPointAnnotation()
@@ -51,8 +33,8 @@ class MapUpdate {
             print("-- MapUpdate -- addPin -- removed pointAnnotation: \(String(describing: pointAnnotationStruct.pointAnnotation))")
         }
             
-        pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: remLatitude,
-                                                            longitude: remLongitude)
+        pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: packet.remoteLatitude,
+                                                            longitude: packet.remoteLongitude)
             
         pointAnnotationStruct.pointAnnotation = pointAnnotation
 
@@ -70,29 +52,45 @@ class MapUpdate {
     func centerView (packet: Location, mapView: MKMapView) -> CLLocationCoordinate2D {
         print("-- MapUpdate -- centerView: center mapView between local and remote users")
     
-        let lLat  = packet.latitude
-        let lLong = packet.longitude
-        let rLat  = packet.remoteLatitude
-        let rLong = packet.remoteLongitude
         var centerLatitude: CLLocationDegrees
         var centerLongitude: CLLocationDegrees
-        let center: CLLocationCoordinate2D
+        var center: CLLocationCoordinate2D
+        let latDistTo: CLLocationDegrees
+        let lngDistTo: CLLocationDegrees
 
-        print("-- MapUpdate -- centerView -- lLat: \(lLat)  lLong: \(lLong) rLat: \(rLat) rLong: \(rLong)")
+        print("-- MapUpdate -- centerView -- lLat: \(packet.latitude)  lLong: \(packet.longitude) rLat: \(packet.remoteLatitude) rLong: \(packet.remoteLongitude)")
 
-        (lLat < rLat) ? (latDistTo = lLat.distance(to: rLat) / 2) :
-                        (latDistTo = rLat.distance(to: lLat) / 2)
-        (lLong < rLong) ? (lngDistTo = lLong.distance(to: rLong) / 2) :
-                        (lngDistTo = rLong.distance(to: lLong) / 2)
+        if packet.remoteLatitude == 0.0 {
+            print("-- MapUpdate -- centerView -- centerLatitude: \(packet.latitude) centerLongitude: \(packet.longitude)")
+            
+            center = CLLocationCoordinate2D(latitude: packet.latitude,
+                                            longitude: packet.longitude)
+
+            mapView.setCenter(center, animated: true)
+
+            return center
+            
+        }
+        
+        // for local and remote location pairs
+        (packet.latitude < packet.remoteLatitude) ?
+                (latDistTo = packet.latitude.distance(to: packet.remoteLatitude) / 2) :
+                (latDistTo = packet.remoteLatitude.distance(to: packet.latitude) / 2)
+
+        (packet.longitude < packet.remoteLongitude) ?
+                (lngDistTo = packet.longitude.distance(to: packet.remoteLongitude) / 2) :
+                (lngDistTo = packet.remoteLongitude.distance(to: packet.longitude) / 2)
         
         print("-- MapUpdate -- centerView -- latDistTo: \(latDistTo) lngDistTo: \(lngDistTo)")
         
-        (lLat > rLat) ? (centerLatitude = rLat + latDistTo) :
-            (centerLatitude = lLat + latDistTo)
+        (packet.latitude > packet.remoteLatitude) ?
+            (centerLatitude = packet.remoteLatitude + latDistTo) :
+            (centerLatitude = packet.latitude + latDistTo)
 
         // negative longitudes
-        (lLong > rLong) ? (centerLongitude = rLong + lngDistTo) :
-            (centerLongitude = lLong + lngDistTo)
+        (packet.longitude > packet.remoteLongitude) ?
+            (centerLongitude = packet.remoteLongitude + lngDistTo) :
+            (centerLongitude = packet.longitude + lngDistTo)
         
         print("-- MapUpdate -- centerView -- centerLatitude: \(centerLatitude) centerLongitude: \(centerLongitude)")
 
@@ -104,14 +102,37 @@ class MapUpdate {
         return center
         
     }
+    
+    func refreshMapView(packet: Location, mapView: MKMapView, delta: Double) {
+        print("-- MapUpdate -- refreshMapView: refresh mapView -- delta: \(delta)")
 
-    func snapView (packet: Location, mapView: MKMapView) {
-        
+            
+        let center = self.centerView(packet: packet, mapView: mapView)
+
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: delta,
+                                                      longitudeDelta: delta)
+
+        let region = MKCoordinateRegion(center: center, span: span)
+
+        mapView.setRegion(region, animated: true)
+
     }
     
+    func displayUpdate(display: UILabel, stringArray: [String]) {
+        print("-- MapUpdate -- displayUpdate")
+        
+        display.text = ""
+        
+        for string in stringArray {
+            display.text =  display.text! + string
+        }
+    }
+
+    /*
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
         renderer.strokeColor = UIColor.blue
         return renderer
     }
+    */
 }
