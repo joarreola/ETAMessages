b671703bf0017a2f970e7bab81ae0074877eb7fb
taiyo <taiyo@taiyos-MacBook-Pro.local>
2017-06-20 15:26:02 -0700

    Add File.

---
+++ MessagesExtension/PseudoNotificationsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PseudoNotificationsViewController.swift
+//  ETAMessages
+//
+//  Created by taiyo on 6/20/17.
+//  Copyright © 2017 Oscar Arreola. All rights reserved.
+//
+
+import UIKit
+
+class PseudoNotificationsViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var message: String = ""
+    
+    override func viewDidLoad() {
+        print("-- PseudoNotificationsViewController -- viewDidLoad()")
+        super.viewDidLoad()
+        
+        self.display.text = self.message
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        print("-- PseudoNotificationsViewController -- didReceiveMemoryWarning()")
+
+    }
+
+}
