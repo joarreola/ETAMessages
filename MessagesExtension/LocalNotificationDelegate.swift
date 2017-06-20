fe77409900c2e5e52690bbdc809455aea259333a
taiyo <taiyo@taiyos-MacBook-Pro.local>
2017-06-20 09:55:52 -0700

    Add the file.

---
+++ MessagesExtension/LocalNotificationDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  LocalNotificationDelegate.swift
+//  ETAMessages
+//
+//  Created by taiyo on 6/19/17.
+//  Copyright © 2017 Oscar Arreola. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class LocalNotificationDelegate: UNUserNotificationCenter,  UNUserNotificationCenterDelegate {
+
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("-- LocalNotificationDelegate -- userNotificationCenter()")
+
+        // notification
+        print("-- LocalNotificationDelegate -- userNotificationCenter() -- notification.request.identifier: \(notification.request.identifier)")
+        
+        // Play a sound.
+        //completionHandler(UNNotificationPresentationOptions.sound)
+        completionHandler(UNNotificationPresentationOptions.alert)
+    }
+
+}
