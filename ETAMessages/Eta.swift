e599607a33672bc4817ad69c65c8ee61a4f62108
taiyo <taiyo@taiyos-MacBook-Pro.local>
2017-06-30 13:46:04 -0700

    Convert EtaAdapter to a Container View. Move etaProgress and progressLabel       objects to EtaAdapter. Create struct ETAIndicator to hold etaProgress and       progressLabel for use in getEtaDistance(). Update getEtaDistance() signature to       remove etaProgress and progressLabel. Add prototype sound-playing code.

--- ETAMessages/Eta.swift
+++ ETAMessages/Eta.swift
@@ -7,7 +7,9 @@
 //
 
 import Foundation
+import UIKit
 import MapKit
+import AVFoundation
 
 /// Adapter to ETA server for eta and distance data
 ///
@@ -15,19 +17,72 @@ import MapKit
 /// distance { get set }
 /// getEtaDistance(Location, Location)
 
-class EtaAdapter {
-    //private var etaPointer: UnsafeMutableRawPointer
-    static var eta: TimeInterval?
-    static var distance: Double?
-    private var mapUdate: MapUpdate
+struct ETAIndicator {
+    var etaProgress: UIProgressView? = nil
+    var progressLabel: UILabel? = nil
+}
+
+class EtaAdapter: UIViewController {
+    static var eta: TimeInterval? = nil
+    static var distance: Double? = nil
+    private var mapUdate: MapUpdate = MapUpdate()
     static var previousDistance: TimeInterval? = nil
     
+    @IBOutlet weak var etaProgress: UIProgressView!
+    
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    static var etaIndicator = ETAIndicator()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("-- EtaAdapter -- viewDidLoad() -----------------------------")
+        
+        self.etaProgress.transform = self.etaProgress.transform.scaledBy(x: 1, y: 7)
+        self.etaProgress.setProgress(Float(0.0), animated: true)
+        
+        // to hold progress and label
+        EtaAdapter.etaIndicator.etaProgress = self.etaProgress
+        EtaAdapter.etaIndicator.progressLabel = self.progressLabel
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("-- EtaAdapter -- viewWillAppear -----------------------------------")
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("-- EtaAdapter -- viewDidAppear ------------------------------------")
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("-- EtaAdapter -- viewWillDisappear --------------------------------")
+        
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("-- EtaAdapter -- viewDidDisappear ---------------------------------")
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        print("-- EtaAdapter -- didReceiveMemoryWarning --------------------------")
+    }
+    
+    //var player: AVAudioPlayer?
+    /*
     init() {
         EtaAdapter.eta = nil
         EtaAdapter.distance = nil
         self.mapUdate = MapUpdate()
 
     }
+    */
 
     func setEta(eta: TimeInterval) {
         EtaAdapter.eta = eta
@@ -53,27 +108,23 @@ class EtaAdapter {
     ///     - etaAdapter: update eta and distance data
     ///     - display: update display content
 
+    func getEtaDistance(localPacket: Location, remotePacket: Location, mapView: MKMapView, display: UILabel,  etaOriginal: TimeInterval) {
+
+        _getEtaDistance (localPacket: localPacket, remotePacket: remotePacket, mapView: mapView, display: display, etaOriginal: etaOriginal)
+    }
+
     func getEtaDistance (localPacket: Location, remotePacket: Location, mapView: MKMapView, display: UILabel) {
-        
-        // no: etaProgressView: UIProgressView, progressDisplay: UILabel, etaOriginal: TimeInterval
-        
-        let etaProgressView: UIProgressView? = nil
-        let progressDisplay: UILabel? = nil
+        // for called by checkRemote() in GPSLocation
+
         let etaOriginal: TimeInterval = 0.0
 
-        _getEtaDistance (localPacket: localPacket, remotePacket: remotePacket, mapView: mapView, display: display, etaProgressView: (etaProgressView), progressDisplay: (progressDisplay), etaOriginal: etaOriginal)
+        _getEtaDistance (localPacket: localPacket, remotePacket: remotePacket, mapView: mapView, display: display, etaOriginal: etaOriginal)
         
-    }
-    
-    func getEtaDistance (localPacket: Location, remotePacket: Location, mapView: MKMapView, display: UILabel, etaProgressView: UIProgressView, progressDisplay: UILabel, etaOriginal: TimeInterval) {
-        
-        // yes: etaProgressView: UIProgressView, progressDisplay: UILabel, etaOriginal: TimeInterval
-        _getEtaDistance (localPacket: localPacket, remotePacket: remotePacket, mapView: mapView, display: display, etaProgressView: etaProgressView, progressDisplay: progressDisplay, etaOriginal: etaOriginal)
     }
 
-    func _getEtaDistance (localPacket: Location, remotePacket: Location, mapView: MKMapView, display: UILabel, etaProgressView: UIProgressView?, progressDisplay: UILabel?, etaOriginal: TimeInterval?) {
+    func _getEtaDistance (localPacket: Location, remotePacket: Location, mapView: MKMapView, display: UILabel, etaOriginal: TimeInterval?) {
         //print("-- EtaAdapter -- getEtaDistance(): get eta from local to remote device," + " and travel distance between devices")
-    
+
         let mkDirReq: MKDirectionsRequest = MKDirectionsRequest()
         
         mkDirReq.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: localPacket.latitude!, longitude: localPacket.longitude!), addressDictionary: nil))
@@ -95,13 +146,7 @@ class EtaAdapter {
                 DispatchQueue.main.async { [weak self ] in
                     
                     if self != nil {
-                        // add pin and refresh mapView
-                        //print("-- EtaAdapter -- getEtaDistance() -- mkDirections.calculate() -- closure -- error -- DispatchQueue.main.async -- closure")
-                        /* will re-span map to a default value resulting in jumpy-map
-                        etaAdapter.mapUdate.addPin(packet: remotePacket, mapView: mapView, remove: false)
-                        
-                        etaAdapter.mapUdate.refreshMapView(localPacket: localPacket, remotePacket: remotePacket, mapView: mapView, eta: etaAdapter)
-                        */
+
                         self?.mapUdate.displayUpdate(display: display, localPacket: localPacket, remotePacket: remotePacket, string: "eta not available", secondString: "distance not available")
                     }
                 }
@@ -172,12 +217,55 @@ class EtaAdapter {
                     //print("-- EtaAdapter -- getEtaDistance() -- mkDirections.calculate() -- closure -- DispatchQueue.main.async -- closure -- etaOriginal: \(String(describing: etaOriginal))")
 
                     if etaOriginal != 0.0 {
-                        etaProgressView?.setProgress(Float(EtaAdapter.eta!) / Float(etaOriginal!), animated: true)
-                    
-                        progressDisplay?.text = "\(Int(EtaAdapter.eta! / 60)) min"
+                        
+                        EtaAdapter.etaIndicator.etaProgress?.setProgress(Float(EtaAdapter.eta!) / Float(etaOriginal!), animated: true)
+
+                        EtaAdapter.etaIndicator.progressLabel?.text = "\(Int(EtaAdapter.eta! / 60)) min"
+
+                        /*
+                        self?.player = self?.loadSound(name: "hornSound")
+                        
+                        if self?.player != nil {
+
+                            self?.player?.play()
+                        }
+                        */
                     }
                 }
             }
         }
     }
+    /*
+    func loadSound(name: String)  -> AVAudioPlayer? {
+        let bundle = Bundle.main
+
+        let optionalPath = bundle.path(forResource: name, ofType: "m4a")
+        
+        if let path = optionalPath {
+
+            let url = URL(fileURLWithPath: path)
+
+            let optionalPlayer = try? AVAudioPlayer(contentsOf: url)
+            
+            if let player = optionalPlayer {
+
+                player.prepareToPlay()
+                
+                print("\(name) loaded.")
+                
+                return player
+            }
+            else {
+                print("Could not load \(name).")
+                
+                return nil
+            }
+        }
+        else {
+            print("Could not create path")
+            
+            return nil
+        }
+    }
+    */
 }
